371c267d0973a2efbf360981220997b6a38c1133
Risky90 <khan.ashankhan@gmail.com>
2023-11-04 16:50:38 +0500

    variables

---
+++ basics_1.js
@@ -0,0 +1,8 @@
+
+const a = 9090; // to declear a constent 
+let b = "Ashan";// to declear a variable
+var c = "Khan"; // to declear a variable (have no scope (old method))
+d = 'bannu';    // to declear a variable (not prefered) 
+/*
+Prefer not to use 'var' bacause of issue in block scope and fuctiona scope.
+*/
