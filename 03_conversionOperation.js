3d6892674253ed2cdf0f021728f86eaef36b64b0
Risky90 <khan.ashankhan@gmail.com>
2023-11-05 10:13:37 +0500

    operations

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -45,10 +45,44 @@ let str = 'Ashan';
 
 
 
-// when string is not empty
-let num = 2;
-let valueInString = String(num);
-console.log(typeof valueInString); // string
-console.log(valueInString); // 2
+// Conversion into string 
+// let num = 2;
+// let valueInString = String(num);
+// console.log(typeof valueInString); // string
+// console.log(valueInString); // 2
 
 
+///////////////// OPERATIONS /////////////////
+// let value = 4;
+// let negvalue = -value;
+// // console.log(negvalue);
+// console.log(2 + 2); // 4
+// console.log(2 - 2); // 0
+// console.log(2 * 2); // 4
+// console.log(2 / 2); // 1
+// console.log(2 % 2); // 0
+// console.log(2 ** 2); // 2 to the power 2 // 4
+// console.log(2 ** 3); // 2 to the power 3 // 8
+
+// let str1 = "Hello";
+// let str2 = " World";
+// let str3 = str1 + str2;
+// console.log(str3);
+
+
+// console.log(1 + "2"); // 12
+// console.log("1" + 2); //12
+// console.log("1" + 2 + 2); //122 (alls convert into string)
+// console.log(1 + 2 + "2"); // 32
+
+// console.log(true) // true
+// console.log(+true) // 1
+// console.log(-true) // -1
+// console.log(true + 1) // 2
+// console.log(1 + true) // 2
+// console.log(-1 + true) // 0
+// console.log(true * 0)  // 0
+
+// let num1 , num2, num3;
+// num1 = num2 = num3 = 2+2;
+// console.log(num1)
