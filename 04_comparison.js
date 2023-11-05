90879f3e0e28c3907234c7e187e5027cb93abdfd
Risky90 <khan.ashankhan@gmail.com>
2023-11-05 15:04:20 +0500

    comparisons

---
+++ 04_comparison.js
@@ -0,0 +1,27 @@
+// console.log(1>2);// false
+// console.log(1>=2);// false
+// console.log(1<2);// true
+// console.log(1==2);// false
+// console.log(1 != 2);// true
+
+// console.log("2" > 1) //ture (string autamatically convert into number)
+// console.log("2" < 1) //false (string autamatically convert into number)
+// console.log("02" > 1)//true  (string autamatically convert into number)
+// console.log("02" == 2)//true  (string autamatically convert into number)
+
+// null
+// console.log(null > 0); //false
+// console.log(null < 0); //false
+// console.log(null == 0); //false
+// console.log(null <= 0); //true
+// console.log(null >= 0); //true
+
+// undefined
+// console.log(undefined > 0); //false
+// console.log(undefined < 0); //false
+// console.log(undefined == 0); //false
+// console.log(undefined <= 0); //false
+// console.log(undefined >= 0); //flase
+
+// === (strict cheak) also cheak datatype
+console.log("2" === 2); // false (it also cheak the datatype)
