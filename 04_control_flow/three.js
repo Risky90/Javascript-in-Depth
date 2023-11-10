14357a530b2de1d59caf017bebaaaacc4f832e12
Risky90 <khan.ashankhan@gmail.com>
2023-11-10 08:14:17 +0500

    for in loop

---
+++ 04_control_flow/three.js
@@ -0,0 +1,36 @@
+
+let myobj = {
+    js: 'javascript',
+    cpp: 'c++',
+    rb: 'ruby',
+    py: 'python'
+}
+// // Syntax
+// for (const key in object) {
+//     if (Object.hasOwnProperty.call(object, key)) {
+//         const element = object[key];
+        
+//     }
+// }
+// for (const key in myobj) {
+// //    console.log(key) // will print all key
+// //    console.log(myobj[key])// will print vlaue of each key at each iterate
+//     console.log(`${key} is the extenson of ${myobj[key]}`)
+// }
+
+// let arr= ['a', 'b', 'c', 'd', 'e']
+// for (const key in arr) {
+//     // console.log(key)// here in for-in loop the indexes of the elements of arr will be print
+//     // console.log(arr[key])// to print its vlaue
+//     console.log(`The vlaue at index ${key} is ${arr[key]}`)
+// }
+
+
+// const map1 = new Map(); 
+// map1.set('a', 'apple');
+// map1.set('b', 'ball');
+// map1.set('c', 'cat');
+
+// for (const key in map1) {
+//    console.log(key) // nothing will ouccer / map is not itratable in for in
+// }
